4caefcbeeb4021543ea209bf828eecea8837b265
Mogilevtsev Dmitry <mogilevtsev.d@garpix.com>
2023-01-27 11:52:54 +0500

    UPD: migrations

--- libs/providers/src/typeorm/typeorm.config.ts
+++ libs/providers/src/typeorm/typeorm.config.ts
@@ -19,7 +19,7 @@ const options = (): DataSourceOptions => {
     entities: [
       join(process.cwd(), 'dist', 'libs', 'entities', '**', '*.entity.{ts,js}'),
     ],
-    migrations: [join(process.cwd(), 'migrations', '**', '*migration.ts')],
+    migrations: [join(process.cwd(), 'migrations', 'js', '*migration.js')],
     migrationsRun: true,
     migrationsTableName: 'migrations',
   };
