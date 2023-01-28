142774abb438173c17ff872d5b85b48be821970a
Mogilevtsev Dmitry <mogilevtsev.d@garpix.com>
2023-01-28 18:20:42 +0500

    ADD: get one post

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,8 +1,10 @@
-import { CurrentUser, ICurrentUser } from '@lib/auth';
+import { CurrentUser, ICurrentUser, Public } from '@lib/auth';
 import { JwtGuard } from '@lib/auth/guards/jwt.guard';
 import { PostFacade } from '@lib/post/application-services';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
+import { Param } from '@nestjs/common/decorators/http/route-params.decorator';
+import { ParseUUIDPipe } from '@nestjs/common/pipes/parse-uuid.pipe';
 import { CreatePostDto } from './dto';
 
 @UseGuards(JwtGuard)
@@ -19,5 +21,11 @@ export class PostController {
       ...createPostDto,
       authorId: user.userId,
     });
+  }
+
+  @Public()
+  @Get(':id')
+  getPostById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.postFacade.queries.getOnePost(id);
   }
 }
