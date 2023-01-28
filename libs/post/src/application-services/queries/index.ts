1d9daa6314f35867b5c5aa7bcc31c44322a6c105
Mogilevtsev Dmitry <mogilevtsev.d@garpix.com>
2023-01-28 14:37:50 +0500

    UPD: queries import

--- libs/post/src/application-services/queries/index.ts
+++ libs/post/src/application-services/queries/index.ts
@@ -1,7 +1,9 @@
 import { Type } from '@nestjs/common';
 import { IQueryHandler } from '@nestjs/cqrs';
 import { GetPostQueryHandler } from './get-post/get-post.query-handler';
+import { GetPostsQueryHandler } from './get-posts/get-posts.query-handler';
 
 export const POST_QUERIES_HANDLERS: Type<IQueryHandler>[] = [
   GetPostQueryHandler,
+  GetPostsQueryHandler,
 ];
