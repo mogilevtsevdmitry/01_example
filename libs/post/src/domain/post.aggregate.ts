7044aa50c9cab0c195bf3688443f89c47ecd9ef6
Mogilevtsev Dmitry <mogilevtsev.d@garpix.com>
2023-01-28 12:42:58 +0500

    ADD: validators

--- libs/post/src/domain/post.aggregate.ts
+++ libs/post/src/domain/post.aggregate.ts
@@ -1,14 +1,37 @@
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import { Exclude } from 'class-transformer';
+import { IsUUID, validateSync } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator/types/decorator/decorators';
 import { IPost } from './post.interface';
 import { PostServices } from './services';
 
 export class PostAggregate extends PostServices implements IPost {
+  @IsUUID()
   id: string = randomStringGenerator();
+
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   message: string;
+
+  @IsUUID()
   authorId: string;
+
+  @IsBoolean()
+  @Exclude()
   published = false;
+
+  @IsString()
   createdAt = new Date().toISOString();
+
+  @IsString()
   updatedAt = new Date().toISOString();
 
   private constructor() {
@@ -18,6 +41,11 @@ export class PostAggregate extends PostServices implements IPost {
   static create(post: Partial<IPost>) {
     const _post = new PostAggregate();
     Object.assign(_post, post);
+    _post.updatedAt = post?.id ? new Date().toISOString() : _post.updatedAt;
+    const errors = validateSync(_post, { whitelist: true });
+    if (!!errors.length) {
+      throw new Error('Post not valid');
+    }
     return _post;
   }
 }
