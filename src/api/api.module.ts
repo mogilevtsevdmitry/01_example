b477766ee74938bba40ec6232932d7346b0abe0e
Mogilevtsev Dmitry <mogilevtsev.d@garpix.com>
2023-01-28 18:15:23 +0500

    UPD: add auth module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,7 +1,8 @@
+import { AuthModule } from '@lib/auth';
 import { Module } from '@nestjs/common';
 import { ControllersModule } from './controllers';
 
 @Module({
-  imports: [ControllersModule],
+  imports: [ControllersModule, AuthModule],
 })
 export class ApiModule {}
--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,22 +1,23 @@
-import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 import { CurrentUser, ICurrentUser } from '@lib/auth';
+import { JwtGuard } from '@lib/auth/guards/jwt.guard';
 import { PostFacade } from '@lib/post/application-services';
 import { Body, Controller, Post } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
 import { CreatePostDto } from './dto';
 
+@UseGuards(JwtGuard)
 @Controller('post')
 export class PostController {
   constructor(private readonly postFacade: PostFacade) {}
 
   @Post()
   createPost(
-    // @CurrentUser() user: ICurrentUser,
+    @CurrentUser() user: ICurrentUser,
     @Body() createPostDto: CreatePostDto,
   ) {
     return this.postFacade.commands.createPost({
       ...createPostDto,
-      authorId: randomStringGenerator(),
-      // authorId: user.userId,
+      authorId: user.userId,
     });
   }
 }
