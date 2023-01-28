c40a2db4fb6b19fa47bb2215ba3446aeeeab5ec2
Mogilevtsev Dmitry <mogilevtsev.d@garpix.com>
2023-01-28 17:01:47 +0500

    ADD: post method: create post

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,7 +1,22 @@
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import { CurrentUser, ICurrentUser } from '@lib/auth';
 import { PostFacade } from '@lib/post/application-services';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePostDto } from './dto';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postFacade: PostFacade) {}
+
+  @Post()
+  createPost(
+    // @CurrentUser() user: ICurrentUser,
+    @Body() createPostDto: CreatePostDto,
+  ) {
+    return this.postFacade.commands.createPost({
+      ...createPostDto,
+      authorId: randomStringGenerator(),
+      // authorId: user.userId,
+    });
+  }
 }
