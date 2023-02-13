e50020872ad25ee60edaa8edd099006ff1966114
mogilevtsevdmitry <webmogilevtsev@ya.ru>
2023-02-13 18:44:26 +0500

    RM

--- libs/post/src/domain/services/set-published.case.ts
+++
@@ -1,9 +0,0 @@
-import { IPost } from './../post.interface';
-
-export interface ISetBublished {
-  setPublished(): void;
-}
-
-export const SET_PUBLISHED = async function (this: IPost) {
-  this.published = true;
-};
